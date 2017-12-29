7fee4bcf865a45bdd900ff2f91f4ecd962986609
Daivy Merlijs <daivymerlijs@MacBook-Pro-de-Daivy.local>
2017-12-29 17:42:28 +0100

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
     todo.save().then((doc) => {
         res.send(doc);
     }, (err) => {
-        res.status(200).send(err);
+        res.status(400).send(err);
 
     });
 
@@ -32,3 +32,7 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 
 });
+
+module.exports = {
+    app
+}
