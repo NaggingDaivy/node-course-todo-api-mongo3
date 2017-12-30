2064b22b52da32c2349a2aef20ba087b864351c2
Daivy Merlijs <daivymerlijs@MacBook-Pro-de-Daivy.local>
2017-12-30 17:17:57 +0100

    Add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -80,6 +82,39 @@ app.delete('/todos/:id', (req, res) => {
     });
 
 });
+
+
+//PATCH /todos/:id
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    var body = _.pick(req.body,['text','completed']); // prendre uniquement les champs nécesasire
+
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+
+    } else {
+
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo)  => { // new renvoie l'objet updaté
+        if(!todo)
+            return res.status(404).send();
+        
+        res.send({todo});
+
+    }).catch((err) => {
+        res.status(400).send();
+    })
+
+})
+
+
 
 app.listen(port, () => {
 
